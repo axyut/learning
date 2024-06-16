1f9a7381372586cc3ac7f8f7da0547ebd3e3c24c
axyut <aoogleak@gmail.com>
2024-06-16 16:08:29 +0545

    q

--- dsa/go/queue/queue.go
+++ dsa/go/queue/queue.go
@@ -1,16 +1,20 @@
 package queue
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 var max_size = 5
-var queues [5]int
-var front = 0
+var queues = make([]int, max_size)
+
+// var front = 0
 var rear = -1 // top
 
 func Run() {
-	fmt.Println("\n------- QUEUE -------")
+	fmt.Println("\nQUEUE")
 begin:
-	fmt.Printf("\n1.Enqueue 2.Dequeue 3. Display 0.Exit\nYour Input: ")
+	fmt.Printf("\n1.Enqueue 2.Dequeue 3. Display 0.Exit\n: ")
 	var num int
 	fmt.Scanln(&num)
 	switch num {
@@ -18,9 +22,11 @@ begin:
 		goto end
 	case 1:
 		enqueue()
+		display()
 		goto begin
 	case 2:
-		dequeue()
+		dequeue_and_organize()
+		display()
 		goto begin
 	case 3:
 		display()
@@ -47,26 +53,58 @@ func enqueue() {
 	} else {
 		fmt.Printf("Enqueue: ")
 		var input int
-		fmt.Scanln(&input)
+		fmt.Scanf("%d", &input)
 
 		rear += 1
 		queues[rear] = input
-		fmt.Println(input, " Enqueued.")
+		fmt.Println("Enqueued:", input)
 	}
 }
 
-func dequeue() {
+//	func dequeue() {
+//		if isEmpty() {
+//			fmt.Println("Queue Empty. Cannot dequeue.")
+//		} else {
+//			fmt.Printf("Dequeued: %d\n", queues[front])
+//			queues[front] = 0
+//			front += 1
+//		}
+//	}
+func dequeue_and_organize() {
 	if isEmpty() {
 		fmt.Println("Queue Empty. Cannot dequeue.")
 	} else {
-		fmt.Printf("delete %d?", queues[front])
-		queues[front] = 0
-		front += 1
+		fmt.Printf("Dequeued: %d\n", queues[0])
+		for i := 0; i <= rear; i++ {
+			if i+1 > max_size-1 {
+				queues[max_size-1] = 0
+			} else {
+				queues[i] = queues[i+1]
+			}
+		}
+		rear -= 1
 	}
 }
 
 func display() {
-	for i, v := range queues {
-		fmt.Println(i, v)
+	for _, v := range queues {
+		for range len(strconv.Itoa(v)) + 3 {
+			fmt.Printf("-")
+		}
 	}
+	fmt.Printf("-")
+	fmt.Printf("\n")
+
+	// main
+	for _, v := range queues {
+		fmt.Printf("| %d ", v)
+	}
+
+	fmt.Printf("|\n")
+	for _, v := range queues {
+		for range len(strconv.Itoa(v)) + 3 {
+			fmt.Printf("-")
+		}
+	}
+	fmt.Printf("-\n")
 }
